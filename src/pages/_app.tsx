677a6332986930255ba66a2e3109c0bc5acae57d
rolikun <roland.kun98@gmail.com>
2023-12-24 00:32:14 +0100

    feat(_app.tsx):used theme & i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { theme } from "@/styles/theme";
+import { ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import "@/utils/i18n/config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
