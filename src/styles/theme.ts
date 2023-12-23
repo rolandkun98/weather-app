ba6eed36ac903d49a2f9aba8537ab623a480e1cd
rolikun <roland.kun98@gmail.com>
2023-12-24 00:31:43 +0100

    feat(styles):added theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material";
+import { createBreakpoints } from "@mui/system";
+
+const breakpoints = createBreakpoints({});
+
+const globalTheme = createTheme({
+  palette: {
+    primary: {
+      light: "#eceff1",
+      main: "#607d8b",
+      dark: "#455a64",
+    },
+  },
+});
+
+export const theme = createTheme({
+  ...globalTheme,
+  typography: {
+    h1: {
+      fontSize: "3.3rem",
+      fontWeight: 600,
+      userSelect: "none",
+      color: globalTheme.palette.primary.dark,
+      [breakpoints.down("sm")]: {
+        fontSize: "2rem",
+      },
+    },
+    subtitle1: {
+      fontSize: "1.2rem",
+      userSelect: "none",
+      color: globalTheme.palette.primary.dark,
+      [breakpoints.down("sm")]: {
+        fontSize: "1rem",
+      },
+    },
+  },
+});
