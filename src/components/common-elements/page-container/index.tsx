de621c0c3b46aad8cafa9653408136286528019c
rolikun <roland.kun98@gmail.com>
2023-12-24 14:31:38 +0100

    feat(page-container):added language changer

--- src/components/common-elements/page-container/index.tsx
+++ src/components/common-elements/page-container/index.tsx
@@ -1,10 +1,15 @@
-import { Box } from "@mui/material";
-
+import { Box, Button } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import LanguageIcon from "@mui/icons-material/Language";
+import { useLanguageHandler } from "@/hooks/use-language-handler";
 interface PageContainerProps {
   children: React.ReactNode;
 }
 
 const PageContainer = ({ children }: PageContainerProps): JSX.Element => {
+  const { i18n } = useTranslation();
+  const { changeLanguage, showOtherLanguage } = useLanguageHandler();
+
   return (
     <Box
       sx={{
@@ -17,6 +22,21 @@ const PageContainer = ({ children }: PageContainerProps): JSX.Element => {
         alignItems: "center",
       }}
     >
+      <Box
+        sx={{
+          width: "100%",
+          paddingRight: "1rem",
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button
+          startIcon={<LanguageIcon />}
+          onClick={() => changeLanguage(i18n.language)}
+        >
+          {showOtherLanguage(i18n.language).toUpperCase()}
+        </Button>
+      </Box>
       {children}
     </Box>
   );
