4c4af36168078f9e26997bcdac639cc9d8338bf7
rolikun <roland.kun98@gmail.com>
2023-12-28 14:24:10 +0100

    feat(endpoints):added open weather map api

---
+++ src/endpoints/open-weather-map/index.ts
@@ -0,0 +1,37 @@
+import { CityData, RawCityData } from "@/utils/interfaces/city";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type GetCityByNameParams = {
+  cityName: string;
+  limit: number;
+};
+
+const openWeatherMapApi = createApi({
+  reducerPath: "openWeatherMapApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://api.openweathermap.org/",
+  }),
+  endpoints: (builder) => ({
+    getCityByName: builder.query<CityData[], GetCityByNameParams>({
+      query: ({ cityName, limit }) =>
+        `geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${process.env.API_KEY}`,
+      transformResponse: (rawData: RawCityData[]) => {
+        return rawData.map((city) => ({
+          name: city.name,
+          latitude: city.lat,
+          longitude: city.lon,
+          country: city.country,
+          localNames: city.local_names,
+        }));
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetCityByNameQuery,
+  useLazyGetCityByNameQuery,
+  reducerPath: openWeatherMapApiReducerPath,
+  reducer: openWeatherMapApiReducer,
+  middleware: openWeatherMapApiMiddleware,
+} = openWeatherMapApi;
