b0b7ca6a25d7fc4f19b32347859499a24eed598a
rolikun <roland.kun98@gmail.com>
2023-12-28 18:18:10 +0100

    feat(_app.tsx):added redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import "@/styles/globals.css";
 import { theme } from "@/styles/theme";
 import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { Provider as ReduxProvider } from "react-redux";
 import "@/utils/i18n/config";
+import store from "@/utils/redux/store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ReduxProvider>
   );
 }
