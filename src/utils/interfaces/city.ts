1d9b0c41a193fe47162e68eb34e26771d14ced62
rolikun <roland.kun98@gmail.com>
2023-12-28 18:19:43 +0100

    feat(interfaces):added city interfaces

---
+++ src/utils/interfaces/city.ts
@@ -0,0 +1,21 @@
+export interface RawCityData {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state?: string;
+  local_names?: {
+    [language: string]: string;
+  };
+}
+
+export interface CityData {
+  name: string;
+  latitude: number;
+  longitude: number;
+  country: string;
+  state?: string;
+  localNames?: {
+    [language: string]: string;
+  };
+}
