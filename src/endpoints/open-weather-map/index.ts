5c565dfbcdf7e8ee17e6799c65a597f54f668d0b
rolikun <roland.kun98@gmail.com>
2023-12-30 16:58:04 +0100

    feat(open-weather-map):added get current weather endpoint

--- src/endpoints/open-weather-map/index.ts
+++ src/endpoints/open-weather-map/index.ts
@@ -1,10 +1,19 @@
 import { CityData, RawCityData } from "@/utils/interfaces/city";
+import {
+  CurrentWeatherData,
+  RawCurrentWeatherData,
+} from "@/utils/interfaces/current-weather";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { v4 as uuidv4 } from "uuid";
 
 type GetCityByNameParams = {
   cityName: string;
   limit: number;
+};
+
+type GetCurrentWeatherByCoordinatesParams = {
+  longitude: number;
+  latitude: number;
 };
 
 const openWeatherMapApi = createApi({
@@ -27,12 +36,32 @@ const openWeatherMapApi = createApi({
         }));
       },
     }),
+    getCurrentWeatherByCoordinates: builder.query<
+      CurrentWeatherData,
+      GetCurrentWeatherByCoordinatesParams
+    >({
+      query: ({ longitude, latitude }) =>
+        `data/2.5/weather?lon=${longitude}&lat=${latitude}&units=metric&appid=${process.env.API_KEY}`,
+      transformResponse: (rawData: RawCurrentWeatherData) => {
+        return {
+          clouds: rawData.clouds.all,
+          windSpeed: rawData.wind.speed,
+          temp: {
+            current: rawData.main.temp,
+            min: rawData.main.temp_min,
+            max: rawData.main.temp_max,
+          },
+        };
+      },
+    }),
   }),
 });
 
 export const {
   useGetCityByNameQuery,
   useLazyGetCityByNameQuery,
+  useGetCurrentWeatherByCoordinatesQuery,
+  useLazyGetCurrentWeatherByCoordinatesQuery,
   reducerPath: openWeatherMapApiReducerPath,
   reducer: openWeatherMapApiReducer,
   middleware: openWeatherMapApiMiddleware,
