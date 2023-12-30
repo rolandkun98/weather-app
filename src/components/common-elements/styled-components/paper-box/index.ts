9d293e8d9035ef7d89174aabe1b5abf99378d828
rolikun <roland.kun98@gmail.com>
2023-12-30 15:10:11 +0100

    feat(styled-components):added PaperBox

---
+++ src/components/common-elements/styled-components/paper-box/index.ts
@@ -0,0 +1,12 @@
+import { Paper } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const PaperBox = styled(Paper)({
+  width: "90%",
+  marginTop: "1.5rem",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  paddingTop: "1rem",
+  paddingBottom: "1rem",
+});
