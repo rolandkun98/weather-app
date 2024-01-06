55efa026eff87dc940193416aac604c85d5198e6
rolikun <roland.kun98@gmail.com>
2024-01-06 15:19:51 +0100

    feat(open-weather-map):added daily forecast

--- src/endpoints/open-weather-map/index.ts
+++ src/endpoints/open-weather-map/index.ts
@@ -1,10 +1,13 @@
 import { CityData, RawCityData } from "@/utils/interfaces/city";
 import {
-  CurrentWeatherData,
+  WeatherData,
   RawCurrentWeatherData,
-} from "@/utils/interfaces/current-weather";
+  RawDailyForecastData,
+} from "@/utils/interfaces/weather";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { v4 as uuidv4 } from "uuid";
+import { format } from "date-fns";
+import { DateFormat } from "@/utils/enums/date-format";
 
 type GetCityByNameParams = {
   cityName: string;
@@ -12,6 +15,11 @@ type GetCityByNameParams = {
 };
 
 type GetCurrentWeatherByCoordinatesParams = {
+  longitude: number;
+  latitude: number;
+};
+
+type GetDailyForecast5DaysParams = {
   longitude: number;
   latitude: number;
 };
@@ -37,21 +45,86 @@ const openWeatherMapApi = createApi({
       },
     }),
     getCurrentWeatherByCoordinates: builder.query<
-      CurrentWeatherData,
+      WeatherData,
       GetCurrentWeatherByCoordinatesParams
     >({
       query: ({ longitude, latitude }) =>
         `data/2.5/weather?lon=${longitude}&lat=${latitude}&units=metric&appid=${process.env.API_KEY}`,
       transformResponse: (rawData: RawCurrentWeatherData) => {
         return {
-          clouds: rawData.clouds.all,
-          windSpeed: rawData.wind.speed,
-          temp: {
-            current: rawData.main.temp,
-            min: rawData.main.temp_min,
-            max: rawData.main.temp_max,
-          },
+          clouds: Math.round(rawData.clouds.all),
+          windSpeed: Math.round(rawData.wind.speed),
+          humidity: Math.round(rawData.main.humidity),
+          temperature: Math.round(rawData.main.temp),
+          weatherDescription: rawData.weather[0].description,
+          icon: rawData.weather[0].icon,
+          date: format(new Date(), DateFormat.PRIMARY),
         };
+      },
+    }),
+    getDailyForecast5Days: builder.query<
+      WeatherData[],
+      GetDailyForecast5DaysParams
+    >({
+      query: ({ longitude, latitude }) =>
+        `data/2.5/forecast?lon=${longitude}&lat=${latitude}&units=metric&appid=${process.env.API_KEY}`,
+      transformResponse: (rawData: RawDailyForecastData) => {
+        const totalForecastData: WeatherData[] = [];
+        const avgForecastData: WeatherData[] = [];
+
+        for (const listItem of rawData.list) {
+          const today = format(new Date(), DateFormat.PRIMARY);
+          const formattedDate = format(
+            new Date(listItem.dt_txt),
+            DateFormat.PRIMARY
+          );
+
+          if (today !== formattedDate) {
+            const foundItem = totalForecastData.find(
+              (totalForecastItem) => totalForecastItem.date === formattedDate
+            );
+
+            if (foundItem) {
+              foundItem.clouds += listItem.clouds.all;
+              foundItem.windSpeed += listItem.wind.speed;
+              foundItem.humidity += listItem.main.humidity;
+              foundItem.temperature += listItem.main.temp;
+            } else {
+              const newTotalForecastItem: WeatherData = {
+                clouds: listItem.clouds.all,
+                windSpeed: listItem.wind.speed,
+                humidity: listItem.main.humidity,
+                temperature: listItem.main.temp,
+                weatherDescription: "",
+                icon: "",
+                date: formattedDate,
+              };
+              totalForecastData.push(newTotalForecastItem);
+            }
+          }
+        }
+
+        for (const totalForecastItem of totalForecastData) {
+          const listItems = rawData.list.filter((listItem) =>
+            listItem.dt_txt.includes(totalForecastItem?.date)
+          );
+          const count = listItems.length;
+
+          const newAvgForecastItem: WeatherData = {
+            clouds: Math.round(totalForecastItem.clouds / count),
+            windSpeed: Math.round(totalForecastItem.windSpeed / count),
+            humidity: Math.round(totalForecastItem.humidity / count),
+            temperature: Math.round(totalForecastItem.temperature / count),
+            weatherDescription:
+              listItems[Math.round(count / 2)].weather[0].description,
+            icon: listItems[Math.round(count / 2)].weather[0].icon,
+            date: totalForecastItem.date.replaceAll("-", "."),
+          };
+
+          avgForecastData.push(newAvgForecastItem);
+        }
+
+        return avgForecastData;
       },
     }),
   }),
@@ -62,6 +135,8 @@ export const {
   useLazyGetCityByNameQuery,
   useGetCurrentWeatherByCoordinatesQuery,
   useLazyGetCurrentWeatherByCoordinatesQuery,
+  useGetDailyForecast5DaysQuery,
+  useLazyGetDailyForecast5DaysQuery,
   reducerPath: openWeatherMapApiReducerPath,
   reducer: openWeatherMapApiReducer,
   middleware: openWeatherMapApiMiddleware,
